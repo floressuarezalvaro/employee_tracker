947fdd10d3edc6fe1c19392e123b81e2e6422ed8
Alvy <floressuarezalvaro@gmail.com>
2020-12-29 20:42:32 -0800

    added inquirer prompt and console logging selected options. Tested each and confirmed they all are hitting correctly

--- tracker.js
+++ tracker.js
@@ -11,6 +11,100 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if (err) throw err;
-    console.log(`You are connected. ID = ${connection.threadId}\n`);
-    connection.end();
+    startPrompt()
 });
+
+const startPrompt = () => {
+    inquirer
+    .prompt({
+        name: 'actions',
+        type: 'list',
+        message: 'Which of these choices would you like to accomplish?',
+        choices: [
+            'View All Employees',
+            'View All Employees by Manager',
+            'View All Roles',
+            'View Departments',
+            'Add Employee',
+            'Add Role',
+            'Add Department',
+            'Update Employee role',
+            'Update Employee Manager',
+            'Exit'
+        ],
+    })
+    .then((answer) => {
+        switch (answer.actions) {
+            case 'View All Employees':
+                viewEmps();
+                break;
+            case 'View All Employees by Manager':
+                viewEmpsByManager();
+                break;
+            case 'View All Roles':
+                viewRoles();
+                break;
+            case 'View Departments':
+                viewDeps();
+                break;
+            case 'Add Employee':
+                addEmps();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'Add Department':
+                addDeps();
+                break;
+            case 'Update Employee role':
+                updateRoles();
+                break;
+            case 'Update Employee Manager':
+                updateEmpManager();
+                break;
+            case 'Exit':
+                connection.end();
+                break;
+            default:
+                console.log("Oops, we did not register your answer, try again.")
+                startPrompt()
+                break;
+        }
+    });
+}
+
+const viewEmps = () => {
+    console.log("You selected: View All Emps")
+}
+
+const viewEmpsByManager = () => {
+    console.log("You selected: View All Employees by Manager")
+}
+
+const viewRoles = () => {
+    console.log("You selected: View All Roles")
+}
+
+const viewDeps = () => {
+    console.log("You selected: viewDeps")
+}
+
+const addEmps = () => {
+    console.log("You selected: addEmps")
+}
+
+const addRoles = () => {
+    console.log("You selected: addRoles")
+}
+
+const addDeps = () => {
+    console.log("You selected: Add Department")
+}
+
+const updateRole = () => {
+    console.log("You selected: updateRoles")
+}
+
+const updateEmpManager= () => {
+    console.log("You selected: updateEmpManager")
+}
